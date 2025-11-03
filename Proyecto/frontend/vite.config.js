8f27054b88de138f07eb68777eac72906cb00687
RaulLlanos <rau.llanos@duocuc.cl>
2025-11-02 23:19:58 -0300

    update index

--- Proyecto/frontend/vite.config.js
+++ Proyecto/frontend/vite.config.js
@@ -5,11 +5,10 @@
 // export default defineConfig({
 //   plugins: [react()],
 // })
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/static/',   // <- importante
-  // ...lo demás
-});
-
+  base: '/static/',
+  plugins: [react()],
+})
