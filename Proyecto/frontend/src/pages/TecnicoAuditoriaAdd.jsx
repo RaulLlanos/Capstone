6d6701ad6c2ef6e800406e30e552d5f31973fdbf
Ekronec <besa.eitan.r@gmail.com>
2025-11-09 18:48:29 -0300

    Se quitó el reagendado en auditoria

--- Proyecto/frontend/src/pages/TecnicoAuditoriaAdd.jsx
+++ Proyecto/frontend/src/pages/TecnicoAuditoriaAdd.jsx
@@ -13,8 +13,7 @@ const CUSTOMER_STATUS = [
   { value: "SIN_MORADORES", label: "Sin Moradores" },
   { value: "RECHAZA", label: "Rechaza" },
   { value: "CONTINGENCIA", label: "Contingencia" },
-  { value: "MASIVO", label: "Masivo" },
-  { value: "REAGENDA", label: "Reagenda" },
+  { value: "MASIVO", label: "Masivo" }
 ];
 
 const SI_NO_NA = [
